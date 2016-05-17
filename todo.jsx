973500638faea8abf1646864fc9028296a10256b
Balrog <da8y_01@yahoo.com>
2016-05-17 12:35:23 -0500

    Adds initial simple react component to test webpack configuration.

---
+++ todo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+class Todo extends React.Component {
+  render() {
+    return <h1>Todo</h1>
+  }
+}
+ 
+ReactDOM.render(<Todo />, document.getElementById('root'));
