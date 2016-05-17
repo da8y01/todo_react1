3f8058a38754fd095263550793845f1b47e064b6
Balrog <da8y_01@yahoo.com>
2016-05-17 13:45:31 -0500

    Adds redux lib, uses redux methods, adds reducers, updates UI component.

--- todo.jsx
+++ todo.jsx
@@ -1,10 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+import { combineReducers } from 'redux';
+import { createStore } from 'redux';
+
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return Object.assign({}, state, {completed: !state.completed});
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+};
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return {};
+  }
+};
+
+
+let nextTodoId = 0;
 class Todo extends React.Component {
   render() {
-    return <h1>Todo</h1>
+    return (
+      <div>
+        <button 
+          onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: 'test',
+              id: nextTodoId++
+            });
+          }}
+        >
+        Add ToDo
+        </button>
+        <ul>
+          <li>first</li>
+          <li>last</li>
+        </ul>
+      </div>
+    );
   }
 }
- 
-ReactDOM.render(<Todo />, document.getElementById('root'));
+
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
+
+const store = createStore(todoApp);
+
+const render = () => {
+  ReactDOM.render(
+    <Todo todos={store.getState().todos} />,
+    document.getElementById('root')
+  );
+};
+
+
+store.subscribe(render);
+render();
